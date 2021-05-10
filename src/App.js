57e7b847c917be857a4717ab16435fac0b54ba7a
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-10 00:45:36 -0300

    importo los estilos de: @material-ui/core/styles; inicio la constante usesStyles como ejemplo; dentro de la funcion App inicio la constante classes; agrego color al h2;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
 import { Fragment } from 'react';
+import MyNavbar from './components/MyNavbar';
+
+const usesStyles = makeStyles((theme) => ({
+  hedings: {
+    color: '#2a324E',
+  },
+}));
 
 function App() {
+  const classes = usesStyles();
   return (
     <Fragment>
+      <MyNavbar />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2 className={classes.hedings}>Encuentra el Cactus o la Suculenta para ti!</h2>
         </header>
       </div>
     </Fragment>
