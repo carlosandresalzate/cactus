36c312ba19b57bd9ec54cc78c05ea3112e2aebe7
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-10 00:49:26 -0300

    importo componetes de  material-ui/core; material-ui/core/AppBar; material-ui/core/Typography; material-ui/core/Button; material-ui/core/IconButton; material-ui/icons/menu; inicializo la constante usesStyles; dentro de la funcion MyNavbar incializo la contante classes;

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,1 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const usesStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,    
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        // color: '#2a324E',
+    },
+    title: {
+        flexGrow: 1,
+        // color: '#2a324E',
+    },
+    color: {
+        backgroundColor: '#f2ebe3',
+        color: '#2a324E',
+    },
+}));
+
+function MyNavbar() {
+    const classes = usesStyles();
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" className={classes.color}>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Cactus
+                    </Typography>
+                    <Button color="inherit">Inicio</Button>
+                    <Button color="inherit">Tienda</Button>
+                    <Button color="inherit">Contacto</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default MyNavbar;
