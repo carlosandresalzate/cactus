030e25e1ba57c1b85088736c173dbc3488c4c91a
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-16 21:26:12 -0300

    remuevo varias lineas compara con git diff; cambios <AppBar> postion=relative;

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import CartWidget from './CartWidget';
@@ -27,11 +26,10 @@ const usesStyles = makeStyles((theme) => ({
 }));
 
 function MyNavbar() {
-    const ListItem = (props) => <Button color="inherit">{ props.item }</Button>;
     const classes = usesStyles();
     return (
         <div className={classes.root}>
-            <AppBar position="fixed" className={classes.color}>
+            <AppBar position="relative" className={classes.color}>
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
@@ -39,13 +37,7 @@ function MyNavbar() {
                     <Typography variant="h6" className={classes.title}>
                         Cactus
                     </Typography>
-                    <ListItem item="Inicio" />
-                    <ListItem item="Tienda" />
-                    <ListItem item="Contacto" />
                     <CartWidget />
-                    {/* <Button color="inherit">Incio</Button>
-                    <Button color="inherit">Tienda</Button>
-                    <Button color="inherit">Contacto</Button> */}
                 </Toolbar>
             </AppBar>
         </div>
