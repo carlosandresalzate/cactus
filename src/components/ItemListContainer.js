9f31bb5868d14f56e3d5f02d6ad11cb9cac46b3c
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-19 00:34:09 -0300

    cambio en las comillas 😅

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Paper, Container} from '@material-ui/core/';
+import { Container, Divider,  Grid, Paper  } from '@material-ui/core/';
+import ItemCount from './ItemCount';
 
 const usesStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        paddingTop: 10,
-    },
-    paper: {
-        textAlign: 'center',
-        padding: 4,
-    }
+  root: {
+    flexGrow: 1,
+    paddingTop: 10,
+  },
+  paper: {
+    textAlign: 'center',
+    padding: 4,
+  },
 }));
 
-
 const ItemListContainer = (props) => {
-    console.log(props);
-    const classes = usesStyles();
-    return (
-        <div className={classes.root}>
-            <Container>
-                <Grid container spacing={3}>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            {props.greeting}
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Container>
-        </div>
-    );
-}
+  console.log(props);
+  const classes = usesStyles();
+  return (
+    <div className={classes.root}>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+              {props.greeting}
+              <Divider orientation='horizontal' />
+              <ItemCount />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default ItemListContainer;
