711f622bed9ea550a74267a48acf81fd7fcdc2f1
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-23 00:55:42 -0300

    agrego componente Button; remuevo componente MakeStyles; agrego parametros a ItemCount({initial, stock, addOn});remuevo stilos; agrego Componente Grid; agrego tag Button;

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,44 +1,47 @@
 import React, { useState } from 'react';
-import { Fab, Grid,  makeStyles } from '@material-ui/core';
+import { Button, Fab, Grid } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 
-const usesStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 5,
-    paddingBottom: 5
-  },
-   buttonColor: {
-     background: '#CA6C73',
-     color: '#f2ebe3',
-     '&:hover': {
-            background: '#E0777E'
-        }
-  }, 
- }));
-
-function ItemCount() {
-  const classes = usesStyles();
-
-  const [count, setCount] = useState(0);
-  console.log(count);
+function ItemCount({ initial, stock, addOn }) {
+  const [count, setCount] = useState(initial);
   return (
-    <Grid container spacing={1} className={classes.root}>
-      <Grid item xs={4}>
-      <Fab size="medium" className={classes.buttonColor} aria-label='remove' onClick={() => (count === 0) ? setCount(0) : setCount(count - 1)}>
+    <Grid container spacing={1} direction='row' justify='space-around'>
+      <Fab
+        color='secondary'
+        size='medium'
+        // className={classes.buttonColor}
+        aria-label='remove'
+        onClick={() =>
+          count === initial ? setCount(count) : setCount(count - 1)
+        }
+      >
         <RemoveIcon />
       </Fab>
-      </Grid>
-      <Grid item xs={4}>
-        <p>{ count }</p>
-      </Grid>
-      <Grid item xs={4}>
-        <Fab size="medium" className={classes.buttonColor} aria-label='add' onClick={() => (count === 10) ? setCount(count) : setCount(count + 1)}>
-          <AddIcon />
-        </Fab>
+      <p>{count}</p>
+
+      <Fab
+        color='secondary'
+        size='medium'
+        // className={classes.buttonColor}
+        aria-label='add'
+        onClick={() =>
+          count !== stock && count < stock
+            ? setCount(count + 1)
+            : setCount(count)
+        }
+      >
+        <AddIcon />
+      </Fab>
+      <Grid item xs={12}>
+        <Button
+          variant='outlined'
+          color='secondary'
+          fullWidth={true}
+          onClick={() => console.log(`el numero de items es ${count}`)}
+        >
+          Agregar
+        </Button>
       </Grid>
     </Grid>
   );
