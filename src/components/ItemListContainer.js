c92de71f89c78a7dd277142ed80fbd82f8ee938a
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-23 00:57:52 -0300

    agrego componentes card, container, y grid; declaro la cantidad de stock y el numero de initial;

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Divider,  Grid, Paper  } from '@material-ui/core/';
-import ItemCount from './ItemCount';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from '@material-ui/core/';
 
-const usesStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    paddingTop: 10,
-  },
-  paper: {
-    textAlign: 'center',
-    padding: 4,
-  },
-}));
+import ItemCount from './ItemCount';
 
 const ItemListContainer = (props) => {
   console.log(props);
-  const classes = usesStyles();
   return (
-    <div className={classes.root}>
-      <Container>
-        <Grid container spacing={3}>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>
-              {props.greeting}
-              <Divider orientation='horizontal' />
-              <ItemCount />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+    <Container style={{ paddingTop: 16 }}>
+      <Grid item xs={3}>
+        <Card>
+          <CardHeader title={props.greeting} />
+          <CardMedia component='img' image='/img/cactus_1.jpg' height=' 140' />
+          <CardContent>
+            <Typography variant='body2' color='textPrimary' component='p'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut rem
+              quis quibusdam eos ipsam sequi itaque.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <ItemCount stock={10} initial={1} />
+          </CardActions>
+        </Card>
+      </Grid>
+    </Container>
   );
 };
 
