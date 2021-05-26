5b3b51cd1cab0644bcbab5b872b904e74ad828cf
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-25 23:23:51 -0300

    agreo componente ItemList dentro del el componente Container;

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,36 +1,12 @@
 import React from 'react';
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Container,
-  Grid,
-  Typography,
-} from '@material-ui/core/';
-
-import ItemCount from './ItemCount';
+import { Container } from '@material-ui/core/';
+import ItemList from './ItemList';
 
 const ItemListContainer = (props) => {
   console.log(props);
   return (
-    <Container style={{ paddingTop: 16 }}>
-      <Grid item xs={3}>
-        <Card>
-          <CardHeader title={props.greeting} />
-          <CardMedia component='img' image='/img/cactus_1.jpg' height=' 140' />
-          <CardContent>
-            <Typography variant='body2' color='textPrimary' component='p'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut rem
-              quis quibusdam eos ipsam sequi itaque.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <ItemCount stock={10} initial={1} />
-          </CardActions>
-        </Card>
-      </Grid>
+    <Container>
+      <ItemList />
     </Container>
   );
 };
