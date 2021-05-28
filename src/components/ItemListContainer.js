6977e7d6c90a45e7e8962a7f461678d8e4935200
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-28 01:51:09 -0300

    agrego componente Mui/core{Box}; tag Box paddingTop{2}

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Container} from '@material-ui/core/';
+import {Container, Box} from '@material-ui/core/';
 import ItemList from './ItemList';
 import ItemDetailContainer from './ItemDetailContainer';
 
 const ItemListContainer = () => {
   return (
     <Container>
-      <ItemDetailContainer />
-      {/* <ItemList /> */}
+      <Box pt={2}>
+        <ItemList />
+      </Box>
     </Container>
   );
 };
