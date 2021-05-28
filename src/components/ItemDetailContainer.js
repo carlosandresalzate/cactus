15aa850bf26081c747834d8168b8a0a47148539f
Carlos Andres Alzate <unarteman@gmail.com>
2021-05-28 01:55:38 -0300

    remuevo componente {Container}; agrego constante singleItem con un filterde item; resolve(singleItem); resuelvo como mostrar data del componente;

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,34 +1,42 @@
 import React, {useEffect, useState} from 'react';
-import {Container, Grid} from '@material-ui/core';
+import {Grid} from '@material-ui/core';
 import {items} from './ItemList';
 import ItemDetail from './ItemDetail';
 
-// console.log(items);
+const singleItem = items.filter((item) => item.id === 1);
 
 const getItem = new Promise((resolve) => {
   setTimeout(() => {
-    if (true) {
-      resolve(items);
-    }
+    resolve(singleItem);
   }, 2000);
 });
-// console.log(getItem);
 
 function ItemDetailContainer() {
   const [itemDetail, setItemDetail] = useState(null);
   useEffect(() => {
     getItem
       .then((response) => response)
-      .then((data) => setItemDetail(data))
+      .then((data) => {
+        setItemDetail(data);
+      })
       .catch((err) =>
         console.log('algo no esta bien en: ' + setItemDetail(err))
       )
       .finally(() => console.info('Promesa cumplida'));
   }, []);
   return (
-    <Container>
-      <h1>{console.log(itemDetail.filter((id, i) => i === 0))}</h1>
-    </Container>
+    <Grid container spacing={2}>
+      {itemDetail?.map((item) => (
+        <Grid item xs={12} sm={6} key={item.id} pt={2}>
+          <ItemDetail
+            title={item.title}
+            pictureUrl={item.pictureUrl}
+            description={item.description}
+            price={item.price}
+          />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
