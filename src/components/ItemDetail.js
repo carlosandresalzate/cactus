19628d9ff8bd196104c3f48e144488a0825a9afc
Carlos Andres Alzate <unarteman@gmail.com>
2021-06-03 23:51:27 -0300

    importo Box; agrego el tag Box;

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -6,36 +6,39 @@ import {
   CardHeader,
   CardMedia,
   Typography,
+  Box,
 } from '@material-ui/core';
 import ItemCount from './ItemCount';
 
 function ItemDetail({ title, description, price, pictureUrl }) {
   return (
-    <Card>
-      <CardHeader title={title} />
-      <CardMedia component='img' image={pictureUrl} height='140' />
-      <CardContent>
-        <Typography
-          variant='body2'
-          color='textPrimary'
-          component='p'
-          align='center'
-        >
-          {description}
-        </Typography>
-        <Typography
-          variant='body1'
-          color='textPrimary'
-          component='p'
-          align='right'
-        >
-          {price}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <ItemCount stock={10} initial={1} />
-      </CardActions>
-    </Card>
+    <Box pt={2}>
+      <Card>
+        <CardHeader title={title} />
+        <CardMedia component='img' image={pictureUrl} height='140' />
+        <CardContent>
+          <Typography
+            variant='body2'
+            color='textPrimary'
+            component='p'
+            align='center'
+          >
+            {description}
+          </Typography>
+          <Typography
+            variant='body1'
+            color='textPrimary'
+            component='p'
+            align='right'
+          >
+            {price}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ItemCount stock={10} initial={1} />
+        </CardActions>
+      </Card>
+    </Box>
   );
 }
 
