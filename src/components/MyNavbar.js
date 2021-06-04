04e2f36b10c6e17299847b394a8ef00e7c215638
Carlos Andres Alzate <unarteman@gmail.com>
2021-06-04 00:06:10 -0300

    agrego comentarios en los imports; importo useState, Link, NavLink, Button; cambio identado🤷‍; agrego tag Button;

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,10 +1,14 @@
-import React from 'react';
+/* React */
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+/* Material */
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+/* Componentes */
 import CartWidget from './CartWidget';
-import { Button } from '@material-ui/core';
 
 const usesStyles = makeStyles((theme) => ({
   root: {
@@ -24,10 +28,23 @@ function MyNavbar() {
     <div className={classes.root}>
       <AppBar position='relative' color='primary'>
         <Toolbar>
-          <Typography variant='h6' className={classes.title}>
+          <Typography
+            variant='h6'
+            className={classes.title}
+            component={NavLink}
+            to='/'
+          >
             Cactus
           </Typography>
-          <Button>
+
+          <Button color='inherit' component={NavLink} to='/category/:id'>
+            ItemListContainer
+          </Button>
+
+          <Button color='inherit' component={NavLink} to='/item/:id'>
+            ItemDetailContainer
+          </Button>
+          <Button component={NavLink} to='/cart'>
             <CartWidget />
           </Button>
         </Toolbar>
